82439ae48e1b5481003f562f80d3edecd86c74bf
Parmeet <parmeetbhamrah@gmail.com>
2026-02-07 11:50:37 +0530

    feat: implement note-taking functionality with input and display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 function App() {
+  const [notes, setNotes] = useState([])
+  const [input, setInput] = useState("")
+
+  const addNote = () => {
+    if (input.trim() === "") return
+    setNotes([...notes, input])
+    setInput("")
+  }
 
   return (
     <div>
+      <h1>Notes</h1>
+
+      <input  
+        value={input}
+        placeholder="Write a note..."
+        onChange= {(e) =>  {setInput(e.target.value)}}
+      />
+
+      <button
+        onClick={addNote}
+      >
+        Add
+      </button>
+
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
     </div>
   )
 }
